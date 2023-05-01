45dc8b4977517ac56bdd98fea189e9846c1880c1
Adam Gardner <gardner.r.adam@gmail.com>
2023-05-01 13:53:13 -0500

    add image and make list a 2x2 grid to display cars

--- app/src/main/java/com/gardner/showmecars/ui/screens/carlist/CarListItem.kt
+++ app/src/main/java/com/gardner/showmecars/ui/screens/carlist/CarListItem.kt
@@ -1,24 +1,50 @@
 package com.gardner.showmecars.ui.screens.carlist
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
+import com.gardner.showmecars.R
 import com.gardner.showmecars.data.remote.dto.Result
 
 @Composable
 fun CarListItem(carResult: Result) {
     val car = carResult.car
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(8.dp)) {
-        Text(text = "${car.make} ${car.model}", fontSize = 20.sp)
-        Spacer(modifier = Modifier.weight(1f))
-        Text(text = "€${carResult.priceInformation.price} per day", fontSize = 20.sp)
+    val imageUrl = car.images?.firstOrNull()
+    
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        SubcomposeAsyncImage(
+            model = imageUrl,
+            contentDescription = "${car.make}",
+            modifier = Modifier
+                .height(150.dp)
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop,
+            loading = { CircularProgressIndicator() },
+            error = {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "An error occurred loading image"
+                )
+            }
+        )
+        
+        Text(text = "${car.make} ${car.model}", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+        Text(text = "€${carResult.priceInformation.price} per day", fontSize = 14.sp)
     }
 }
--- app/src/main/java/com/gardner/showmecars/ui/screens/carlist/CarListScreen.kt
+++ app/src/main/java/com/gardner/showmecars/ui/screens/carlist/CarListScreen.kt
@@ -6,6 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -41,7 +44,8 @@ fun CarListScreen(
             label = { Text(text = "Search city") },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .padding(16.dp),
+            singleLine = true
         )
     
         LaunchedEffect(searchQuery) {
@@ -50,7 +54,8 @@ fun CarListScreen(
                 viewModel.searchCars(searchQuery, "NL")
             }
         }
-        LazyColumn(
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
             modifier = Modifier.fillMaxSize(),
             contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
         ) {
